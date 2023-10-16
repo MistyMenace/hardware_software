709c9a0e6384b82d5ad759d637dedbffcfc282ff
MistyMenace <Jacobpvzgw@gmail.com>
2023-10-16 13:16:19 -0400

    updating after class

---
+++ BegginingModification.py
@@ -0,0 +1,7 @@
+name="Professor Nedd"
+print("We want to know if you like programming")
+Print()
+print( "We want to know if you like programming" + name + "?")
+anwser = input()
+print("Great! You said" + anwser + "?")
+print("Lets learn some Python today")
---
+++ addNumbers.py
@@ -0,0 +1,11 @@
+def main ():
+      number1 = 125
+      number2 = 100
+      number3 = number1 - number2
+      print (number3)
+      number3 = 10
+      number1 = 20
+      number2 = number1 + number2 + number3
+      print(number3 + number2)
+if__name_=="__main__":
+   main()
